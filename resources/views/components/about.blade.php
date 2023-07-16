033b6ec0da71a798c6fa9c97791c1a8393a3654f
junaid-mij <junath57@gmail.com>
2023-07-17 00:08:28 +0600

    new

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -6,7 +6,7 @@
                 <div class="text-center my-5">
                     <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2>
                     <p id="about-title" class="lead fw-light mb-4">My name is Junaid and I help brands grow.</p>
-                    <p id="about-details" class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit dolorum itaque qui unde quisquam consequatur autem. Eveniet quasi nobis aliquid cumque officiis sed rem iure ipsa! Praesentium ratione atque dolorem?</p>
+                    <p id="about-details" class="text-muted">Lorem ipsum dolor sit amet, consectetu adipisicing elit. Fugit dolorum itaque qui unde quisquam consequatur autem. Eveniet quasi nobis aliquid cumque officiis sed rem iure ipsa! Praesentium ratione atque dolorem?</p>
                     <div class="d-flex justify-content-center fs-2 gap-4">
                         <a target="_blank" id="twitter" class="text-gradient" href=""><i class="bi bi-twitter"></i></a>
                         <a target="_blank" id="linkedin" class="text-gradient" href=""><i class="bi bi-linkedin"></i></a>
